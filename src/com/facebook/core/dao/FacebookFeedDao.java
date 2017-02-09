43dd2b48480efeebab577ec3a35c61d31c1d388a
Mihaylov <denis.mihaylov@sap.com>
2017-02-09 16:30:37 +0200

    Creates the view feed servlet

--- src/com/facebook/core/dao/FacebookFeedDao.java
+++ src/com/facebook/core/dao/FacebookFeedDao.java
@@ -1,5 +1,16 @@
 package com.facebook.core.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.facebook.core.model.FacebookFeedEntry;
+
 public class FacebookFeedDao extends BaseDao {
+	
+	public List<FacebookFeedEntry> getUserFacebookFeedEntries(int userId) {
+		List<FacebookFeedEntry> result = new ArrayList<>();
+		
+		return result;
+	}
 
 }
